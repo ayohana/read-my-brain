230c7342fb7afb5a2913f6aa6d6590a28d470779
Adela Darmansyah <adela.yohana@gmail.com>
2020-05-20 16:10:13 -0700

    Resolve warnings in the console

--- src/components/About.js
+++ src/components/About.js
@@ -7,7 +7,7 @@ function App(props) {
       <div id="aboutContainer">
         <h2>What Brain?</h2>
         <p>Not just any brain -- a <em>nursing</em> brain.</p>
-        <p>So what are these brains? Nursing 'brain' sheets -- or simply known as <strong><a href="https://allnurses.com/nursing-brain-sheets-beginners-t566048/">brains</a></strong>, are report sheets that contain handwritten information, most often chicken scratch, to remember about their patients. A patient's diagnosis, medical history, lab values, medications, treatment schedule, and discharge plans are among many things a nurse has to write down within just a few minutes during change of shift. They have to start writing down a bunch of shorthands and acronyms to write everything down more efficiently.</p>
+        <p>So what are these brains? Nursing 'brain' sheets -- or simply known as <strong><a href="https://allnurses.com/nursing-brain-sheets-beginners-t566048/" target="_blank" rel="noopener noreferrer">brains</a></strong>, are report sheets that contain handwritten information, most often chicken scratch, to remember about their patients. A patient's diagnosis, medical history, lab values, medications, treatment schedule, and discharge plans are among many things a nurse has to write down within just a few minutes during change of shift. They have to start writing down a bunch of shorthands and acronyms to write everything down more efficiently.</p>
 
         <h2>Whose Brain?</h2>
         <p>This application aims to empower future nurses and caregivers by helping them understand medical charts quicker, better, easier and less stressful!</p>
@@ -15,8 +15,8 @@ function App(props) {
         <h4 onClick={() => props.onClickingGetStarted(true)}>Start <span className="appName">readmybrain</span> now!</h4>
 
         <h3>Found a bug, broken feature or a brain fart?</h3>
-        <h4><a href="mailto:adela.yohana@gmail.com" target="_blank">Let me know!</a></h4>
-        <h4><a href="https://github.com/ayohana/" target="_blank">Source: GitHub repo</a></h4>
+        <h4><a href="mailto:adela.yohana@gmail.com" target="_blank" rel="noopener noreferrer">Let me know!</a></h4>
+        <h4><a href="https://github.com/ayohana/" target="_blank" rel="noopener noreferrer">Source: GitHub repo</a></h4>
       </div>
     </React.Fragment>
   );
--- src/components/Modal.js
+++ src/components/Modal.js
@@ -10,7 +10,7 @@ function Modal(props) {
         </div>
         <div id="modalContent">
           <p>Thanks for using <span className="appName">readmybrain</span>! Here's a little friendly reminder for you.</p>
-          <p>Ever heard of <a className="modalLink" href="https://www.hhs.gov/hipaa/for-individuals/guidance-materials-for-consumers/index.html" target="_blank">HIPAA</a>? Please be aware of the contents of your brain especially if it contains real patient private information.  If you must, don't forget to turn on private browsing on your device! </p>
+          <p>Ever heard of <a className="modalLink" href="https://www.hhs.gov/hipaa/for-individuals/guidance-materials-for-consumers/index.html" target="_blank" rel="noopener noreferrer">HIPAA</a>? Please be aware of the contents of your brain especially if it contains real patient private information.  If you must, don't forget to turn on private browsing on your device! </p>
           <p>Thanks for agreeing to protecting patients' privacy!</p>
         </div>
       </div>
