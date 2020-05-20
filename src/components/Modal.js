d2588de0e7833e7599f19ae5825a8c6d9012e438
Adela Darmansyah <adela.yohana@gmail.com>
2020-05-20 11:20:16 -0700

    Style modal content

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -7,8 +7,8 @@ function Modal(props) {
         <div id="modalContent">
           <span id="closeIcon" onClick={() => props.onClosingModal(true)}>&times;</span>
           <h2>Hello there, future caregiver!</h2>
-          <h3>Thanks for using Read My Brain! Here's a little friendly reminder for you:</h3>
-          <h3>Please be aware of the contents of your brain especially if it contains real patient private information. Ever heard of <a className="modalLink" href="https://www.hhs.gov/hipaa/for-individuals/guidance-materials-for-consumers/index.html" target="_blank">HIPAA</a>? If you must, don't forget to turn on private browsing on your device! </h3>
+          <h3>Thanks for using Read My Brain! Here's a little friendly reminder for you.</h3>
+          <h3>Ever heard of <a className="modalLink" href="https://www.hhs.gov/hipaa/for-individuals/guidance-materials-for-consumers/index.html" target="_blank">HIPAA</a>? Please be aware of the contents of your brain especially if it contains real patient private information.  If you must, don't forget to turn on private browsing on your device! </h3>
           <h3>Thanks for agreeing to protecting patients' privacy!</h3>
         </div>
       </div>
